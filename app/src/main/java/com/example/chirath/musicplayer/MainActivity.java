e7fd3b2c6d6115f563fe68532fc0b026263956f1
chirath <chirath.02@gmail.com>
2016-08-05 22:41:05 +0530

    Completed seekbar

--- app/src/main/java/com/example/chirath/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/chirath/musicplayer/MainActivity.java
@@ -90,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                mediaPlayer.pause();
+                mediaPlayer.start();
             }
         });
     }
